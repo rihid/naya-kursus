b444aea571c04d8580c5316608d7263e872e7195
rizal-haw <rizalhidayat006@gmail.com>
2023-02-01 16:18:21 +0700

    Penggunaan backslash

--- 03-02/halo.js
+++ 03-02/halo.js
@@ -4,22 +4,23 @@ a = 3;
 console.log(a);
 
 let b = 1;
-// let b = 2; (Tidak bisa dijalankan karena sudah dideklarasikakn sebelumnya)
+// let b = 2; //(Tidak bisa dijalankan karena sudah dideklarasikakn sebelumnya)
 b = 3;
 console.log(b);
 
 const c = 1;
-// const c = 2; (Tidak bisa dijalankan karena sudah dideklarasikakn sebelumnya)
-// c = 3; (Tidak bisa dijalankan kareana sudah ditugaskan sebagai variabel constatnt)
+// const c = 2; //(Tidak bisa dijalankan karena sudah dideklarasikakn sebelumnya)
+// c = 3; //(Tidak bisa dijalankan kareana sudah ditugaskan sebagai variabel constatnt)
 console.log(c);
 
 const iyoOra = true;
 console.log(typeof iyoOra)
 
-const nama = 'Rizal'
-const umur = 17
+const nama = 'Rizal';
+const umur = 10.5;
+const greeting = 'I\'m ' + nama + ' and i\'m ' + umur + ' yrs old.';
 
-console.log(`Halo nama saya adalah ${nama} dan umur saya adalah ${umur} tahun`)
-console.log(`I'm ${nama} and I'm ${umur} yrs old.`)
+console.log(`Halo nama saya adalah ${nama} dan umur saya adalah ${umur} tahun`);
+console.log(greeting);
 
 
