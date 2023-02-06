9bbc8b6f15effbf792c8c9632fa1eef9871c6d0d
rizal-haw <rizalhidayat006@gmail.com>
2023-02-06 13:28:40 +0700

    Pengambilan keputusan

---
+++ 04-01/catatan.md
@@ -0,0 +1,14 @@
+# Pengambilan Keputusan (Decision Making)
+
+1. Konsep percabangan
+2. Percabangan dengan if, if-else, if-elseif
+3. Percabangan dengan switch-case
+
+Struktur penulisan sintaks If = if (){}
+
+Kurung biasa diisi kondisi dengan kodisi, sementara kurung kurawal untuk block kode pengambilan keputusan yang kita mau.
+
+## Konsep Switch Case
+Switch case pada dasarnya memiliki konsep seperti saklar
+
+Defaultnya, switch case akan menjalankan baris kode yang sesuai dengan case-nya dan apapun di bawahnya, untuk menghentikan itu maka diperlukan break;. Break akan menghentikan switch case dan langsung ke penutup kurawal dan mengulangi begitu terus.
---
+++ 04-01/main.js
@@ -0,0 +1,81 @@
+// Convert Nilai
+
+let nilai = 79;
+
+if (nilai >= 80){
+    console.log('A');
+}
+else if (nilai >= 60 && nilai < 80){
+    console.log('B');
+}
+else if (nilai >= 40 && nilai < 60){
+    console.log('C');
+}
+else if (nilai >= 1 && nilai < 40){
+    console.log('D');
+}
+else{
+    console.log('E');
+}
+
+// Convert juga tapi pakai Switch Case
+
+// Rumus-Rumus
+
+// Rumus Lingkaran
+
+let phi = 3.14;
+let r1 = 3;
+let r2 = 4;
+
+let luasLingkaran1 = phi * r1**2;
+let luasLingkaran2 = phi * r2**2;
+
+if (luasLingkaran1 == luasLingkaran2){
+    console.log("Sama");
+}
+else{
+    console.log("Tidak Sama")
+}
+
+
+// Rumus Persegi panjang
+
+let p1 = 6;
+let l1 = 3;
+
+let p2 = 6;
+let l2 = 3;
+
+let luasPersegi1 = p1 * l1;
+let luasPersegi2 = p2 * l2;
+
+if (luasPersegi1 == luasPersegi2){
+    console.log("Sama");
+}
+else{
+    console.log("Tidak Sama");
+}
+
+// Rumus Segitiga 
+
+let a1 = 2;
+let t1 = 3;
+
+let a2 = 2;
+let t2 = 4;
+
+let luasSegtiga1 = a1 * t1;
+let luasSegtiga2 = a2 * t2;
+
+
+if (luasSegitiga1 == luasSegitiga2){
+    console.log("Sama");
+}
+else{
+    console.log("Tidak Sama");
+}
+
+// Rumus Volume Balok
+
+
