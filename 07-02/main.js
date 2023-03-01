48862a65d86a9f7896282e2fc07dd5fc82cda04b
rizal-haw <rizalhidayat006@gmail.com>
2023-03-01 12:32:02 +0700

    Menampilkan tabular data di console dari data tabel di HTML

---
+++ 07-02/main.js
@@ -0,0 +1,27 @@
+// let tableData = document.getElementsByTagName('table');
+
+// console.log(tableData)
+
+function dataTable() {
+    var table = document.getElementById("tabel_siswa");
+    var header = [];
+    var rows = [];
+
+    for (var i = 0; i < table.rows[0].cells.length; i++) {
+        header.push(table.rows[0].cells[i].innerHTML);
+    }
+    console.log(header)
+    for (var i = 1; i < table.rows.length; i++) {
+        var row = {};
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            row[header[j]] = table.rows[i].cells[j].innerHTML;
+        }
+        rows.push(row);
+        // console.log(row)
+    }
+
+    console.log(rows);
+}
+dataTable();
+
+
---
+++ 07-02/readme.md
@@ -0,0 +1,6 @@
+Pembahasan:
+
+1. Menambah elemen baru
+2. Mengubah teks
+3. Menambah style
+4. Menghapus elemen
