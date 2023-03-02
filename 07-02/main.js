d7569ae602409e468f498ca0dceb616af85d038e
rizal-haw <rizalhidayat006@gmail.com>
2023-03-02 08:50:56 +0700

    Menampilkan abular data

--- 07-02/main.js
+++ 07-02/main.js
@@ -1,26 +1,26 @@
-// let tableData = document.getElementsByTagName('table');
 
-// console.log(tableData)
+// TUgas Menampilkan tabel html ke console
 
 function dataTable() {
     var table = document.getElementById("tabel_siswa");
     var header = [];
     var rows = [];
-
+    // Loop isi <tr> pada indeks 0
     for (var i = 0; i < table.rows[0].cells.length; i++) {
         header.push(table.rows[0].cells[i].innerHTML);
     }
-    console.log(header)
+    // Loop isi <tr> mulai dari indeks 1
     for (var i = 1; i < table.rows.length; i++) {
         var row = {};
+        // Loop Array of Object
         for (var j = 0; j < table.rows[i].cells.length; j++) {
             row[header[j]] = table.rows[i].cells[j].innerHTML;
         }
         rows.push(row);
-        // console.log(row)
     }
+    // rows.push(row);
 
-    console.log(rows);
+    console.table(rows);
 }
 dataTable();
 
--- 07-02/readme.md
+++ 07-02/readme.md
@@ -1,6 +1,17 @@
+# Manipulasi DOM
+
 Pembahasan:
 
 1. Menambah elemen baru
 2. Mengubah teks
 3. Menambah style
 4. Menghapus elemen
+
+## Menambah element baru
+
+Bisa dengan menggunakan document.createElement('h1)
+menambahkan teks pada element yang dibuat bisa dengan .innerTEXT
+menambahkan clas pada element yang dibuat dengan .className
+menambahkan id pada element yang dibuat dengan .id
+untuk menambah style dengan .style
+untuk menghapus element dengan .remove()
