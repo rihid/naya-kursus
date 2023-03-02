c2c72bd36eaa2580542cfba235556c13b98d4d17
rizal-haw <rizalhidayat006@gmail.com>
2023-03-03 00:58:16 +0700

    Membuat tabel dari DOM dan menampilkan gambar dengan DOM

--- 07-02/main.js
+++ 07-02/main.js
@@ -20,8 +20,64 @@ function dataTable() {
     }
     // rows.push(row);
 
-    console.table(rows);
+    return(rows);
 }
-dataTable();
+
+let isi = dataTable();
+console.table(isi);
+
+// Tabel dari JS
+
+let container = document.getElementsByClassName('container');
+let tbl = document.createElement('table');
+container[0].appendChild(tbl);
+tbl.id = 'tblcustom';
+
+
+function tHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+}
+
+function tBody(table, obj) {
+    let tbody = table.createTBody();
+    for (let element of obj) {
+      let row = tbody.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+}
+  
+let tblCustom = document.getElementById("tblcustom");
+let data = Object.keys(isi[0]);
+tHead(tblCustom, data);
+tBody(tblCustom, isi);
+
+
+// Menampilkan Gambar
+
+let imgContainer = document.getElementsByClassName('image');
+let img = document.createElement('img');
+imgContainer[0].appendChild(img);
+img.src = './img/degods.jpg'
+img.style = `
+width:10rem;
+border-radius: 50%;
+`
+
+
+
+
+
+
 
 
