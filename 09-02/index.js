84d217e4d1e5de5d9496758cac344e1c6ba6789a
rizal-haw <rizalhidayat006@gmail.com>
2023-03-24 12:40:29 +0700

    update fetch api

--- 09-02/index.js
+++ 09-02/index.js
@@ -2,6 +2,7 @@ const api = axios.create({
     baseURL: "https://dummyjson.com"
 });
 
+// Single Post
 api.get("posts/9")
 .then( res => {
     $(".content > h1").text(res.data.title);
@@ -11,10 +12,43 @@ api.get("posts/9")
 api.get("comments/post/9")
 .then( res => {
     res.data.comments.forEach( cmts => {
-        const div = $(".comment");
+        let div = $(".comment");
         div.append(`
         <p>@${cmts.user.username}</p>
         <p>${cmts.body.replace("good", "***")}</p>
         `);
     })
 })
+
+// All Posts
+
+api.get("posts/")
+.then( res => {
+    res.data.posts.forEach( post => {
+        let container = $('.content-all');
+        container.append(`
+            <h1><a href="#" style="color:black;">${post.title}</a></h1>
+            <p>${post.body}</p>
+        `)
+        // console.log(post.title);
+    })
+});
+
+// Post Method
+
+const form = document.querySelector('#post-form')
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(form)
+    formData.append('userId', 9)
+
+    api.post('posts/add', formData)
+    .then(res => {console.log(res)})
+    .catch(err => err)
+        
+})
+
+
+
+
+
--- 10-02/js/index.js
+++ 10-02/js/index.js
@@ -1,12 +1,21 @@
+// Fetch Api
 const api = axios.create({
     baseURL: "https://dummyjson.com"
 });
 
-var x = {
-    posts : [],
+// Read Api
+let x = {
+    datas : [],
     init(){
-        api.get("posts/")
-        .then(res => x.posts = res.data);
+        return api.get("posts/")
+            .then(res => {
+                return res.data.posts;
+            });
     }
-    
 }
+
+// Single Post
+
+
+
+
